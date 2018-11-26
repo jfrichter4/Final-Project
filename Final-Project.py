c68d792c147f9f65b7daf2ee8a5adb74aaea3d2e
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-11-26 13:05:04 -0500

    Updated from Brython Server: 11/26/2018 1:05:05 PM

--- Final-Project.py
+++ Final-Project.py
@@ -1,6 +1,13 @@
 """
 Put the credit and stuff here:
 """
-Welcome = input("Welcome to the Oversimplified GPA Calc! What do you want to do?")
-Question = input("Okay. Is your GPA weighted or unweighted?")
-Then = input
+print("Welcome to the Oversimplified GPA Calc!")
+Question = input("Is your GPA weighted or unweighted?")
+Then = input("Okay - I understand. Enter your grades in this form: A;A-;B;C;D;F;N/A?")
+print("This is your GPA: 4")
+"""
+Now, make a zip list compiling A:4,A-:3.67, etc,.
+Then, have something with <Question> that makes the code know what weighted means and what unweighted means.
+After, have something with <Then> that adds the numbers together and then divides them over how many grades there were.
+Finally, have something with <After> that displays the GPA.
+"""
