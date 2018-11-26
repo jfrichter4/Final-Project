d6828513e7e0bd197f29f9e54d796f81321cb00b
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-11-26 12:51:30 -0500

    Updated from Brython Server: 11/26/2018 12:51:31 PM

--- Final-Project.py
+++ Final-Project.py
@@ -1,1 +1,6 @@
-
+"""
+Put the credit and stuff here:
+"""
+Welcome = input("Welcome to the Oversimplified GPA Calc! What do you want to do?")
+Question = input("Okay. Is your GPA weighted or unweighted?")
+Then = input
