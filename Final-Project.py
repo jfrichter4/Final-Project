c668fa2d14243a3b6d10418f8d7e05b854fb4b48
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-12-11 12:32:18 -0500

    Updated from Brython Server: 12/11/2018 12:32:22 PM

--- Final-Project.py
+++ Final-Project.py
@@ -48,15 +48,21 @@ elif After == "N":
     print("Thanks!")
 """
 Zip1 = zip((A:100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80, 79, 78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50)
-"""
-Zip1 = zip("A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F+", "F", "F-", "U", "N/A")
-Zip2 = zip(4, 4, 3.67, 3.33, 3, 2.67, 2.33, 2, 1.67, 1.33, 1, .67, .33, 0)
+
+Zip1 = ("A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F+", "F", "F-", "U", "N/A")
+Zip2 = (4, 4, 3.67, 3.33, 3, 2.67, 2.33, 2, 1.67, 1.33, 1, .67, .33, 0)
+Zip3 = zip(Zip1, Zip2)
+
 Total = sum("A", "B", "C", "D", "E", "F", "G", "H")
-Final = Total/J
+"""
+Total = {"A+":4, "A":4, "A-":3.67, "B+":3.33, "B":3, "B-":2.67, "C+":2.33, "C":2, "C-":1.67, "D+":1.33, "D":1, "D-":0.67, "F+":0.33, "F":0, "F-":0, "U":0, "N/A":0}
+
+Final = sum(Total)
+Printed = Final/J
 if Final > 3.7:
-    print("You are above average - your GPA is " + Final + "!")
+    print("You are above average - your GPA is " + Printed + "!")
 if Final < 3.7:
-    print("You are below average - your GPA is " + Final + "!")
+    print("You are below average - your GPA is " + Printed + "!")
 
 """
 Now, make a zip list compiling A:4,A-:3.67, etc,.
